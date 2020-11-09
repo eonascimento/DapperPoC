184b0a8e87733b393e7b9f8b636df0cd6555aa8a
Ênio Oliveira <eonascimento@stefainini.com>
2020-11-08 23:56:30 -0300

    Add project files.

---
+++ DataLayer/ContactRepository.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataLayer
+{
+    public class ContactRepository : IContactRepository
+    {
+        private IDbConnection db;
+        public ContactRepository(string connString)
+        {
+            this.db = new SqlConnection(connString);
+        }
+        public Contact Add(Contact contact)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Contact Find(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Contact> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Contact Update(Contact contact)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Runner/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace Runner
+{
+    class Program
+    {
+        private static IConfiguration config;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void initialize()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            config = builder.Build();
+        }
+
+    }
+}
